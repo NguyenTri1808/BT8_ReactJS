fd796eb768f4319140792560e6c8f641e351c680
TriNguyen <tringuyen.2372004@gmail.com>
2025-06-03 09:20:07 +0700

    First commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bulma/css/bulma.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import 'bulma/css/bulma.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
